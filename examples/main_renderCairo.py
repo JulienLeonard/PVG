b2664ca52367c4bfd04de19f7e1b795f5fb5166e
julienleonard <jl.julien.leonard@gmail.com>
2015-08-21 18:01:01 +1000

    Implement Cairo renderer

---
+++ examples/main_renderCairo.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0, './../lib')
+
+from renderCairo import *
+from color       import *
+
+outputfilepath = defaultoutputdir() + "/" + "main_rendCairo.png"
+render = RenderCairo(Viewport(Point(0.0,0.0),5.0),ImageDim(1000,1000),outputfilepath, Color.red())
+render.drawcircle(Circle(Point(0.0,0.0),1.0),Color.green())
+render.end()
