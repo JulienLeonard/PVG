73446851611c2b343a1177b4b457393b25f8f798
julienleonard <jl.julien.leonard@gmail.com>
2015-08-22 10:11:25 +1000

    Add example for SVG renderer

---
+++ examples/main_renderSVG.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0, './../lib')
+
+from renderSVG import *
+from color     import *
+
+outputfilepath = defaultoutputdir() + "/" + "main_renderSVG.svg"
+render = RenderSVG(Viewport(Point(0.0,0.0),2.0),ImageDim(500,500),outputfilepath, Color.white())
+render.drawcircle(Circle(Point(0.0,0.0),1.0),Color.green())
+render.end()
