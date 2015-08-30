74a7609fa9cbf2b1c853023e17b7524ca6232892
julienleonard <jl.julien.leonard@gmail.com>
2015-08-31 09:25:59 +1000

    Upgrade example

--- examples/main_curvepoints.py
+++ examples/main_curvepoints.py
@@ -1,19 +1,12 @@
-# some_file.py
-import sys
-sys.path.insert(0, './../lib')
+from PVG          import *
 
-from utils      import *
-from canvas     import *
-from SVGparser  import *
-from color      import *
-
-render = Canvas("main_curvepoints.png").render()
+canvas = Canvas()
 
 paths  = svgPaths("flower.svg")
 silhouettes = [path.silhouette() for path in paths]
 for (silhouette,ccolor) in lzip(silhouettes,[Color.green(0.5),Color.red(0.5),Color.yellow(0.5)]):
-    render.drawpolygon(silhouette.polygon(),ccolor)
-    render.drawcircles([Circle(p,10.0) for p in silhouette.polygon().points(100)],ccolor.a(1.0))
+    canvas.draw(silhouette.polygon(),ccolor)
+    canvas.draw([Circle(p,10.0) for p in silhouette.polygon().points(100)],ccolor.a(1.0))
 
 
-render.end()
+canvas.save("main_curvepoints.png")
