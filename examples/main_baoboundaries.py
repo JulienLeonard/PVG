e222b893e0af5405c07c20648ed74b90fa4e95c7
julienleonard <jl.julien.leonard@gmail.com>
2015-09-01 10:25:40 +1000

    Update baoboundaries with new boundary management

--- examples/main_baoboundaries.py
+++ examples/main_baoboundaries.py
@@ -6,12 +6,13 @@ canvas = Canvas()
 def fdraw(node,index,style):
     canvas.draw(node,style)
 
-boundaries = [Circle(p,1.0) for p in C0.scale(100.0).samples(usamples(300))]
+boundaries = [Circle(p,1.0) for p in Circle(Point(0.0,98.0),100.0).samples(usamples(300))]
 inodes     = baonodes0()
 baopattern = BaoPattern().fdraw(fdraw).radiuspattern(R(1.0,3.0).samples(20)).colorpattern([Color.hue2color(float(index)/20.0) for index in range(20)])
 
 canvas.draw(inodes)
+canvas.draw(boundaries)
 
-packing = CirclePackingBao.iter(boundaries,inodes,baopattern,3000)
+packing = CirclePackingBao.iter(boundaries,inodes,baopattern,5000)
 
 canvas.save("baoboundaries.png")
