fde4ce78a54b04444e8df08f6ff5665b885dbf1d
julienleonard <jl.julien.leonard@gmail.com>
2015-09-24 20:29:11 +1000

    Upgrade example to debug baolayer

--- examples/main_circlepackingbaolayer.py
+++ examples/main_circlepackingbaolayer.py
@@ -3,20 +3,26 @@ from PVGPACK                import *
 
 canvas = Canvas()
 
+rnlayer = Roller([i for i in range(20)])
 def fdrawlayer(nodes):
     puts("nnodes",len(nodes))
-    hues = R(0.0,1.0).samples(2*len(nodes))
+    hues = [float(rnlayer.next() % 4)/4.0] * len(nodes)
     for (node,hue) in zip(nodes,hues):
         canvas.draw(node,Color.hue2color(hue))
+
+def fdraw(node,index,style):
+    canvas.draw(node,style)
 
 poly = Circle().scale(100.0).polygon(30).close()
 canvas.draw(poly,Color.black())
 boundaries = poly.segments()
 inodes     = BaoNode.fromcircle(Circle(Point(0.0,95.0),10.0))
 baopattern = BaoPatternLayer().fdrawlayer(fdrawlayer).radiuspattern(R(5.0,5.0).samples(20))
+# baopattern = BaoPattern().fdraw(fdraw).radiuspattern(R(5.0,5.0).samples(20)).colorpattern([Color.hue2color(float(index)/20.0) for index in range(20)])
 
 # canvas.draw(inodes)
 
+# packing = CirclePackingBao.iter(boundaries,inodes,baopattern,200,-1.0)
 packing = CirclePackingBaoLayer.iter(boundaries,inodes,baopattern,500,-1.0)
 
 canvas.save("circlepackingbaolayer.png")
